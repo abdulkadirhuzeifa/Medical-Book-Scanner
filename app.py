d8204187be6997712be006d027d0822faa308f7c
AbdulkadirH5253 <100148154+AbdulkadirH5253@users.noreply.github.com>
2023-08-02 19:14:59 +0300

    edit1

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 # Import PDF document loaders...there's other ones as well!
 from langchain.document_loaders import PyPDFLoader
-# Import chroma as the vector store 
+# Import chroma as the vector store
 from langchain.vectorstores import Chroma
 
 # Import vector store stuff
@@ -20,23 +20,24 @@ from langchain.agents.agent_toolkits import (
 
 # Set APIkey for OpenAI Service
 # Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = 'youropenaiapikeyhere'
+os.environ['OPENAI_API_KEY'] = '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'
 
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
 embeddings = OpenAIEmbeddings()
 
 # Create and load PDF Loader
-loader = PyPDFLoader('annualreport.pdf')
-# Split pages from pdf 
+loader = PyPDFLoader('hawwamedicine.pdf')
+# Split pages from pdf
 pages = loader.load_and_split()
 # Load documents into vector database aka ChromaDB
-store = Chroma.from_documents(pages, embeddings, collection_name='annualreport')
+store = Chroma.from_documents(
+    pages, embeddings, collection_name='hawwamedicine')
 
 # Create vectorstore info object - metadata repo?
 vectorstore_info = VectorStoreInfo(
-    name="annual_report",
-    description="a banking annual report as a pdf",
+    name="hawwamedicine",
+    description="a medicine book as a pdf",
     vectorstore=store
 )
 # Convert the document store into a langchain toolkit
@@ -48,7 +49,7 @@ agent_executor = create_vectorstore_agent(
     toolkit=toolkit,
     verbose=True
 )
-st.title('🦜🔗 GPT Investment Banker')
+st.title('Hawwa Medical Assistant')
 # Create a text input box for the user
 prompt = st.text_input('Input your prompt here')
 
@@ -59,9 +60,9 @@ if prompt:
     # ...and write it out to the screen
     st.write(response)
 
-    # With a streamlit expander  
+    # With a streamlit expander
     with st.expander('Document Similarity Search'):
         # Find the relevant pages
-        search = store.similarity_search_with_score(prompt) 
-        # Write out the first 
-        st.write(search[0][0].page_content) 
+        search = store.similarity_search_with_score(prompt)
+        # Write out the first
+        st.write(search[0][0].page_content)
